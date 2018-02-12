f8f3e0ddc14d852e7337f9d249848f32686b8742
Lauri-Matti Parppei <lauri-matti@kapitan.fi>
2018-02-12 11:34:26 +0200

    First commit

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ let mainWindow
 
 function createWindow () {
 
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 1200, height: 850})
 
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
